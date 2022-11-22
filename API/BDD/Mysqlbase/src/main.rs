4831f24414322acb19f62f1c64f71dcb5eee2f05
thomaschok <96106035+thomaschok@users.noreply.github.com>
2022-11-22 19:40:49 +0100

    Update main.rs

--- API/BDD/Mysqlbase/src/main.rs
+++ API/BDD/Mysqlbase/src/main.rs
@@ -1,63 +1,84 @@
 use mysql::*;
 use mysql::prelude::*;
-
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 #[derive(Debug, PartialEq, Eq)]
-struct Payment {
-    customer_id: i32,
-    amount: i32,
-    account_name: Option<String>,
+struct Password {
+    login: String,
+    passw: u64,
 }
 
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let url = "mysql://GAGA:mypass@localhost:3306/BEFROST";
-    let pool = Pool::new(url)?;
+impl Hash for Password {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.login.hash(state);
+        self.passw.hash(state);
+    }
+}
 
+
+
+fn PASSWORDHASH() {
+	let person1 = Password {
+    	login: "Thomas".to_string(),
+    	passw: 0000, 
+	};
+    
+assert_eq!(calculate_hash(&person1),calculate_hash(&person1));
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+
+}
+
+println!("{}", calculate_hash(&person1))   
+
+}
+  
+
+fn main ()-> std::result::Result<(), Box<dyn std::error::Error>>{
+     let url = "mysql://GAGA:mypass@localhost:3306/passwd";
+    let pool = Pool::new(url)?;
     let mut conn = pool.get_conn()?;
+    PASSWORDHASH();
 
     // Let's create a table for payments.
     conn.query_drop(
-        r"CREATE TABLE payment (
-            customer_id int not null,
-            amount int not null,
-            account_name text
+        r"CREATE TABLE password (
+            login text not null,
+            password int not null
         )")?;
 
-    let payments = vec![
-        Payment { customer_id: 1, amount: 2, account_name: None },
-        Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
-        Payment { customer_id: 5, amount: 6, account_name: None },
-        Payment { customer_id: 7, amount: 8, account_name: None },
-        Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
+    let passwords = vec![
+        Password { login:"Thomas".to_string(), passw: 0000 },
     ];
 
     // Now let's insert payments to the database
     conn.exec_batch(
-        r"INSERT INTO payment (customer_id, amount, account_name)
-          VALUES (:customer_id, :amount, :account_name)",
-        payments.iter().map(|p| params! {
-            "customer_id" => p.customer_id,
-            "amount" => p.amount,
-            "account_name" => &p.account_name,
+        r"INSERT INTO password (login, password)
+          VALUES (:login, :password)",
+        passwords.iter().map(|p| params! {
+            "login" => &p.login,
+            "password" => p.passw,
         })
     )?;
 
     // Let's select payments from database. Type inference should do the trick here.
-    let selected_payments = conn
+    let selected_passwords = conn
         .query_map(
-            "SELECT customer_id, amount, account_name from payment",
-            |(customer_id, amount, account_name)| {
-                Payment { customer_id, amount, account_name }
+            "SELECT login, password from password",
+            |(login, passw)| {
+                Password { login, passw }
             },
         )?;
 
     // Let's make sure, that `payments` equals to `selected_payments`.
     // Mysql gives no guaranties on order of returned rows
     // without `ORDER BY`, so assume we are lucky.
-    assert_eq!(payments, selected_payments);
+    assert_eq!(passwords, selected_passwords);
     println!("Yay!");
 
-    Ok(())
- 
-    
-}
+    Ok(())  
+ }
