66507406adc4ab11633cdd9710136c99b7f25d1f
Hugo30128 <104615072+Hugo30128@users.noreply.github.com>
2022-11-29 14:56:17 +0100

    casefinal

--- Others/PartieHugo/codeexemple.rs
+++ Others/PartieHugo/codeexemple.rs
@@ -1,28 +1,35 @@
 use std::io;
 
 fn main() {
-    println!("voici le menu");
-    println!("1) Afficher coucou");
-    println!("2) Afficher thomas");
-    println!("3) Afficher Jai taff tas vu ?");
-    println!("4) Exit");
-    println!("Que voulez vous faire ?");
+     println!("voici le menu");
+     println!("1) Afficher coucou");
+     println!("2) Afficher thomas");
+     println!("3) Afficher Jai taff tas vu ?");
+     println!("4) Exit");
+     
+    
+     
 
-    let mut choix = String::new();
-   
-    loop{
+     loop{ 
+
+        let mut input = String::new();
         io::stdin()
-        .read_line(&mut choix)
-        .expect("Error");
+        .read_line(&mut input).expect("error: unable to read user input");
 
-     match choix.as_str(){
-        "1"=>println!("coucou"),
-        "2"=>println!("thomas"),
-        "3"=>println!("Jai taff tas vu ?"),
-        _=>println!("Entrer non valide"),
+        let input: u32 = input.trim().parse().expect("Veuillez entrer un nombre !");
+
+   
+        match input{
+        1=>println!("coucou"),
+        2 =>println!("thomas"),
+        3 =>println!("Jai taff tas vu ?"),
+        4 =>println!("Exit"),
+         _ =>println!("Choix non valide"),
         }
-    if choix=="4"{
-        break;
+
+        if input == 4{
+            break;
+        }
     }
-    }
+
 }
