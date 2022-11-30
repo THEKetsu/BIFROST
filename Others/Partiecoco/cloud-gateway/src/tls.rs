12600636b207ed5a78ed3a239436fcb8b87269d2
Aimasu59 <corentintyteca@gmail.com>
2022-11-30 09:39:32 +0100

    Ajout cloud gateway v1

---
+++ Others/Partiecoco/cloud-gateway/src/tls.rs
@@ -0,0 +1,70 @@
+use std::{fs::File, io::BufReader};
+use actix_web::{http::header::ContentType,HttpRequest, HttpResponse};
+use log::debug;
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+
+/// simple handle
+pub async fn index(req: HttpRequest) -> HttpResponse {
+    debug!("{req:?}");
+
+    HttpResponse::Ok().content_type(ContentType::html()).body(
+        "<!DOCTYPE html><html><body>\
+            <p>Welcome to your TLS-secured homepage!</p>\
+        </body></html>",
+    )
+}
+/*
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    let config = load_rustls_config();
+
+    log::info!("starting HTTPS server at https://localhost:8443");
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // register simple handler, handle all methods
+            .service(web::resource("/index.html").to(index))
+            .service(redirect("/", "/index.html"))
+            .service(Files::new("/static", "static"))
+    })
+    .bind_rustls("127.0.0.1:8443", config)?
+    .run()
+    .await
+}
+*/
+
+pub fn load_rustls_config() -> rustls::ServerConfig {
+    // init server config builder with safe defaults
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    // load TLS key/cert files
+    let cert_file = &mut BufReader::new(File::open("Certificate/127.0.0.1+1.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("Certificate/127.0.0.1+1-key.pem").unwrap());
+
+    // convert files to key/cert objects
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // exit if no keys could be parsed
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
+}
